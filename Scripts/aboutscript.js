e359bcb0e10bb1cde5dc045fd8c83948c53ae3a2
1DannyGitH <61436532+1DannyGitH@users.noreply.github.com>
2020-03-08 19:54:55 -0600

    Add files via upload

---
+++ Scripts/aboutscript.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    var canvas = this.document.getElementById('barGraph');
+    var ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+    ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+ctx.fillRect(25,475,50,25);
+    ctx.fillStyle = "#C0C5C1";
+}
