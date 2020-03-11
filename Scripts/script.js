8ba01a0a85c727eb3f13b4c01fdae4c08702784e
randomizerkatt <ABroderick@student.neumont.edu>
2020-03-11 11:55:50 -0600

    Update script.js

--- Scripts/script.js
+++ Scripts/script.js
@@ -48,3 +48,13 @@ $(document).ready(() => {
     
 });
 
+var KEYCODE_I = 73;
+
+function handleIPress(evt) {
+    var tasd = evt.keyCode;
+    if(evt.keyCode === KEYCODE_I) {
+        alert("We all scream for ice cream!");
+    }
+}
+
+document.onkeydown = handleIPress;
