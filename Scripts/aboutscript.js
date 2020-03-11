92c987ff6da374d55297a620e769cdbdb1004512
The64GigKid <47188661+The64GigKid@users.noreply.github.com>
2020-03-11 11:53:20 -0600

    About Page
    Put the graph in the aboutPage.js

--- Scripts/aboutscript.js
+++ Scripts/aboutscript.js
@@ -1,17 +1,23 @@
+//Bar Graph Code
+
 window.onload = function(){
     var canvas = this.document.getElementById('barGraph');
     var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
-    ctx.fillRect(25,475,50,25);
-    ctx.fillStyle = "#C0C5C1";
+
+    //Bar One
+    ctx.fillStyle = "#00FFF7";
+    ctx.fillRect(20,275,100,300);
+
+    //Bar Two
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(215,100,100,450);
+
+    //Bar Three
+    ctx.fillStyle = "#00FF7F";
+    ctx.fillRect(415,135,100,400);
+
+    //Bar Four
+    ctx.fillStyle = "#FFC0CB";
+    ctx.fillRect(650,225,100,300);
+    //Bar Graph 5
 }
--- Scripts/script.js
+++ Scripts/script.js
@@ -48,24 +48,3 @@ $(document).ready(() => {
     
 });
 
-//Bar Graph Code
-
-window.onload = function(){
-    var canvas = this.document.getElementById('barGraph');
-    var ctx = canvas.getContext('2d');
-    ctx.fillStyle = "#00FFF7";
-    ctx.fillRect(20,275,100,300);
-
-    //Bar two
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(215,100,100,450);
-
-  //Bar Three
-    ctx.fillStyle = "#00FF7F";
-    ctx.fillRect(415,135,100,400);
-
-    //Bar Four
-    ctx.fillStyle = "#FFC0CB";
-    ctx.fillRect(650,225,100,300);
-    //Bar Graph 5
-}
