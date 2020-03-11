d203a6bba1b77f4a0d14dc1f2e205c8c0966f9dc
randomizerkatt <ABroderick@student.neumont.edu>
2020-03-11 17:38:52 -0600

    Doing one level thing

--- Scripts/contact.js
+++ Scripts/contact.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
           console.log(response);
           document.getElementById("quote").innerHTML = response.contents.quotes[0].quote
           document.getElementById("qTitle").innerHTML = response.contents.quotes[0].title
+          console.log(response.contents.quotes[0].background);
           document.getElementById("qback").style.backgroundImage = "url("+response.contents.quotes[0].background+")"
         }
     });
