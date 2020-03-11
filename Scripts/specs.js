0a41206d92c075cc6123e6b9dfdf2147976fa64b
The64GigKid <47188661+The64GigKid@users.noreply.github.com>
2020-03-11 01:44:14 -0600

    Table update
    higlights the row instead of indvidual items

--- Scripts/specs.js
+++ Scripts/specs.js
@@ -1,3 +1,176 @@
-$(document).ready(function(){
-    document.getElementById("cold").innerHTML = "30 &deg;F"
+$(document).ready(function () {
+    cold1.onmouseover = function () {
+        cold1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cold1.style.color = "white"
+        cold2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cold2.style.color = "white"
+    };
+    cold2.onmouseover = function () {
+        cold1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cold1.style.color = "white"
+        cold2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cold2.style.color = "white"
+    };
+    cold1.onmouseout = function () {
+        cold1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cold1.style.color = "black"
+        cold2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cold2.style.color = "black"
+    };
+    cold2.onmouseout = function () {
+        cold1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cold1.style.color = "black"
+        cold2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cold2.style.color = "black"
+    };
+
+    cream1.onmouseover = function () {
+        cream1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cream1.style.color = "white"
+        cream2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cream2.style.color = "white"
+    };
+    cream2.onmouseover = function () {
+        cream1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cream1.style.color = "white"
+        cream2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cream2.style.color = "white"
+    };
+    cream1.onmouseout = function () {
+        cream1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cream1.style.color = "black"
+        cream2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cream2.style.color = "black"
+    };
+    cream2.onmouseout = function () {
+        cream1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cream1.style.color = "black"
+        cream2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cream2.style.color = "black"
+    };
+
+    milk1.onmouseover = function () {
+        milk1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        milk1.style.color = "white"
+        milk2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        milk2.style.color = "white"
+    };
+    milk2.onmouseover = function () {
+        milk1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        milk1.style.color = "white"
+        milk2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        milk2.style.color = "white"
+    };
+    milk1.onmouseout = function () {
+        milk1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        milk1.style.color = "black"
+        milk2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        milk2.style.color = "black"
+    };
+    milk2.onmouseout = function () {
+        milk1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        milk1.style.color = "black"
+        milk2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        milk2.style.color = "black"
+    };
+
+    cal1.onmouseover = function () {
+        cal1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cal1.style.color = "white"
+        cal2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cal2.style.color = "white"
+    };
+    cal2.onmouseover = function () {
+        cal1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cal1.style.color = "white"
+        cal2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        cal2.style.color = "white"
+    };
+    cal1.onmouseout = function () {
+        cal1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cal1.style.color = "black"
+        cal2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cal2.style.color = "black"
+    };
+    cal2.onmouseout = function () {
+        cal1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cal1.style.color = "black"
+        cal2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        cal2.style.color = "black"
+    };
+
+    fat1.onmouseover = function () {
+        fat1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        fat1.style.color = "white"
+        fat2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        fat2.style.color = "white"
+    };
+    fat2.onmouseover = function () {
+        fat1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        fat1.style.color = "white"
+        fat2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        fat2.style.color = "white"
+    };
+    fat1.onmouseout = function () {
+        fat1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        fat1.style.color = "black"
+        fat2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        fat2.style.color = "black"
+    };
+    fat2.onmouseout = function () {
+        fat1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        fat1.style.color = "black"
+        fat2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        fat2.style.color = "black"
+    };
+
+    sugar1.onmouseover = function () {
+        sugar1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        sugar1.style.color = "white"
+        sugar2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        sugar2.style.color = "white"
+    };
+    sugar2.onmouseover = function () {
+        sugar1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        sugar1.style.color = "white"
+        sugar2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        sugar2.style.color = "white"
+    };
+    sugar1.onmouseout = function () {
+        sugar1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        sugar1.style.color = "black"
+        sugar2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        sugar2.style.color = "black"
+    };
+    sugar2.onmouseout = function () {
+        sugar1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        sugar1.style.color = "black"
+        sugar2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        sugar2.style.color = "black"
+    };
+
+    size1.onmouseover = function () {
+        size1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        size1.style.color = "white"
+        size2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        size2.style.color = "white"
+    };
+    size2.onmouseover = function () {
+        size1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        size1.style.color = "white"
+        size2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Rocky.png')"
+        size2.style.color = "white"
+    };
+    size1.onmouseout = function () {
+        size1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        size1.style.color = "black"
+        size2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        size2.style.color = "black"
+    };
+    size2.onmouseout = function () {
+        size1.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        size1.style.color = "black"
+        size2.style.backgroundImage = "url('../Images/Ice\ Cream\ Scoops/Vanilla.png')"
+        size2.style.color = "black"
+    };
 });
